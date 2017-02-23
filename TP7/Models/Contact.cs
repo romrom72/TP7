8b89844906d64c7e3eddb78107a4517081ffb23b
Titin <Titin@TITIN>
2017-02-23 14:16:56 +0100

    MAJ Model <Contact>

--- TP7/Models/Contact.cs
+++ TP7/Models/Contact.cs
@@ -19,44 +19,5 @@ namespace TP7.Models
             setEmail(email);
             setPhone(phone);
         }
-
-        public String getFirstName()
-        {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName)
-        {
-            this.firstName = firstName;
-        }
-
-        public String getLastName()
-        {
-            return lastName;
-        }
-
-        public void setLastName(String lastName)
-        {
-            this.lastName = lastName;
-        }
-
-        public String getEmail()
-        {
-            return email;
-        }
-
-        public void setEmail(String email)
-        {
-            this.email = email;
-        }
-        public String getPhone()
-        {
-            return phone;
-        }
-
-        public void setPhone(String phone)
-        {
-            this.phone = phone;
-        }
     }
 }
